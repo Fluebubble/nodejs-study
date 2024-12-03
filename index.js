69230e028a2c3cbc2e051ea7794245e69b8433ec
Fluebubble <Trots.anatoly@gmail.com>
2024-12-03 18:15:16 +0100

    init node with module system

---
+++ index.js
@@ -0,0 +1,11 @@
+import os from "os";
+import path from "path";
+// import { add, substract } from "./actions.js";
+
+// const { add, substract } = require("./actions");
+
+// console.log(module.exports);
+
+console.log(
+  path.resolve('./package.json')
+);

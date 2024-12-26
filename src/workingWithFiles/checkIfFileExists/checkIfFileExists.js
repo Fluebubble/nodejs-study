06f06f8f6b8eef4ce417942dc63577ab690add9d
Fluebubble <Trots.anatoly@gmail.com>
2024-12-26 19:10:36 +0100

    block finished

--- src/workingWithFiles/checkIfFileExists/checkIfFileExists.js
+++ src/workingWithFiles/checkIfFileExists/checkIfFileExists.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url); // так можно сделать полный путь к файлу в ES модулях
 const __dirname = path.dirname(__filename); // так можно сделать полный путь к папке файла в ES модулях
-console.log(__filename, __dirname);
+console.log(`${__dirname} \n${__filename}`);
 
 const filePath = path.join(__dirname, "../../../public/1.txt");
 

0a8fba1e9546ac54136cf1c30a2bc845ba3b7d5c
Fluebubble <Trots.anatoly@gmail.com>
2025-01-09 16:58:05 +0100

    nodemon and custom port tested

---
+++ src/httpModule/nodemonAndCustomPort/nodemonAndCustomPort.js
@@ -0,0 +1,13 @@
+import http from "http";
+
+const PORT = process.env.PORT || 3002;
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-type", "text/plain");
+  res.write("<h1>123123</h1>");
+  res.end("hello world");
+});
+
+server.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}/`);
+});
